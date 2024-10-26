4d61e2c1919ed7301d498f344383f4d5de54b926
HusanboyMansurov <husanboymansurov78@gmail.com>
2024-10-26 22:05:12 +0500

    Added secret PORT

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -15,8 +17,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World! PORT = 3000"', () => {
+      expect(appController.getHello()).toBe('Hello World! PORT = 3000');
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    console.log(process.env.PORT);
+
+    return `Hello World! PORT = ${process.env.PORT}`;
   }
 }
